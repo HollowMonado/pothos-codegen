31ced75b40fd6ac5fd81f9dcd6dec9a348b51882
Tin Srnić <tin.srnic@ember-coding.com>
2025-05-22 17:35:38 +0200

    Simplified query resolver

--- src/crudGenerator/templates/query.ts
+++ src/crudGenerator/templates/query.ts
@@ -1,81 +1,160 @@
-import { useTemplate } from "utils/template.js";
-import { makeResolver } from "./resolver.js";
+import { firstLetterLowerCase } from "utils/string.js";
 
-const queryNames = ["findFirst", "findMany", "count", "findUnique"] as const;
-type OperationOptions = (typeof queryNames)[number];
+export type QueryOperation = "findFirst" | "findMany" | "count" | "findUnique";
 
-const makeQuery = (
-    operation: OperationOptions,
-    type: string,
-    nullable: "true" | "false",
-    isPrisma = true,
-    args?: string,
-    resolve?: string
-) =>
-    makeResolver(
-        "Query",
-        "",
-        operation,
-        type,
-        nullable,
-        args ??
-            useTemplate(queryListArgsTemplate, {}, [
-                "modelName",
-                "modelNameUpper",
-            ]),
-        resolve ??
-            useTemplate(
-                queryListResolveTemplate,
-                {
-                    distinct:
-                        operation === "count"
-                            ? ""
-                            : "\n        distinct: args.distinct || undefined,",
-                    operation,
-                    query: isPrisma ? "\n        ...query," : "",
-                    argsQuery: isPrisma ? "query, " : "",
-                },
-                ["prisma", "modelNameLower"]
-            ),
-        isPrisma
-    );
+function makeQueryTemplate({
+    queryOperation,
+    modelName,
+    argsReturn,
+    type,
+    nullable,
+    resolve,
+}: {
+    queryOperation: QueryOperation;
+    modelName: string;
+    argsReturn: string;
+    type: string;
+    nullable: boolean;
+    resolve: string;
+}) {
+    return `import * as Inputs from '../../inputs';
+import { builder } from '../../../builder';
 
-const queryListArgsTemplate = `{
-  where: t.field({ type: Inputs.#{modelName}WhereInput, required: false }),
-  orderBy: t.field({ type: [Inputs.#{modelName}OrderByWithRelationInput], required: false }),
-  cursor: t.field({ type: Inputs.#{modelName}WhereUniqueInput, required: false }),
+export const ${queryOperation}${modelName}QueryArgs = builder.args((t) => (${argsReturn}))
+
+export const ${queryOperation}${modelName}QueryObject: QueryPrismaObject = {
+  type: ${type},
+  nullable: ${nullable},
+  args: ${queryOperation}${modelName}QueryArgs,
+  resolve: ${resolve},
+};
+`;
+}
+
+function makeQueryArgsTemplate({ modelName }: { modelName: string }) {
+    return `{
+  where: t.field({ type: Inputs.${modelName}WhereInput, required: false }),
+  orderBy: t.field({ type: [Inputs.${modelName}OrderByWithRelationInput], required: false }),
+  cursor: t.field({ type: Inputs.${modelName}WhereUniqueInput, required: false }),
   take: t.field({ type: 'Int', required: false }),
   skip: t.field({ type: 'Int', required: false }),
-  distinct: t.field({ type: [Inputs.#{modelNameUpper}ScalarFieldEnum], required: false }),
+  distinct: t.field({ type: [Inputs.${modelName}ScalarFieldEnum], required: false }),
 }`;
+}
 
-const queryListResolveTemplate = `async (#{argsQuery}_root, args, _context, _info) =>
-      await #{prisma}.#{modelNameLower}.#{operation}({
+function makeQueryResloverTemplate({
+    prismaName,
+    isPrisma,
+    modelName,
+    operation,
+    hasDistinct,
+}: {
+    prismaName: string;
+    isPrisma: boolean;
+    modelName: string;
+    operation: QueryOperation;
+    hasDistinct: boolean;
+}) {
+    const modelNameLower = firstLetterLowerCase(modelName);
+
+    const queryArg = isPrisma ? "query" : "";
+    const query = isPrisma ? "\n...query" : "";
+
+    return `async (${queryArg}_root, args, _context, _info) =>
+      await ${prismaName}.${modelNameLower}.${operation}({
         where: args.where || undefined,
         cursor: args.cursor || undefined,
-        take: args.take || undefined,#{distinct}
+        take: args.take || undefined,${hasDistinct ? "distinct: args.distinct || undefined," : ""}
         skip: args.skip || undefined,
-        orderBy: args.orderBy || undefined,#{query}
+        orderBy: args.orderBy || undefined,${query}
       })`;
+}
 
-const querySingleArgsTemplate = `{ where: t.field({ type: Inputs.#{modelName}WhereUniqueInput, required: true }) }`;
+function makeFindUniqueArgsTemplate({ modelName }: { modelName: string }) {
+    return `{ where: t.field({ type: Inputs.${modelName}WhereUniqueInput, required: true }) }`;
+}
 
-const querySingleResolveTemplate = `async (query, _root, args, _context, _info) =>
-      await #{prisma}.#{modelNameLower}.findUnique({ where: args.where, ...query })`;
+function makeFindUniqueResloverTemplate({
+    prismaName,
+    modelName,
+}: {
+    prismaName: string;
+    modelName: string;
+}) {
+    const modelNameLower = firstLetterLowerCase(modelName);
 
-const findFirst = makeQuery("findFirst", "'#{modelName}'", "true");
+    return `async (query, _root, args, _context, _info) =>
+      await ${prismaName}.${modelNameLower}.findUnique({ where: args.where, ...query })`;
+}
 
-const findMany = makeQuery("findMany", "['#{modelName}']", "false");
+export function makeFindFirst({ modelName }: { modelName: string }) {
+    return makeQueryTemplate({
+        queryOperation: "findFirst",
+        modelName: modelName,
+        argsReturn: makeQueryArgsTemplate({ modelName }),
+        type: "'#{modelName}'",
+        nullable: false,
+        resolve: makeQueryResloverTemplate({
+            prismaName: "prisma",
+            isPrisma: true,
+            modelName,
+            operation: "findFirst",
+            hasDistinct: true,
+        }),
+    });
+}
 
-const count = makeQuery("count", "'Int'", "false", false);
+export function makeFindMany({ modelName }: { modelName: string }) {
+    return makeQueryTemplate({
+        queryOperation: "findMany",
+        modelName: modelName,
+        argsReturn: makeQueryArgsTemplate({ modelName }),
+        type: "['#{modelName}']",
+        nullable: true,
+        resolve: makeQueryResloverTemplate({
+            prismaName: "prisma",
+            isPrisma: true,
+            modelName,
+            operation: "findMany",
+            hasDistinct: true,
+        }),
+    });
+}
 
-const findUnique = makeQuery(
-    "findUnique",
-    "'#{modelName}'",
-    "true",
-    true,
-    useTemplate(querySingleArgsTemplate, {}, ["modelName"]),
-    useTemplate(querySingleResolveTemplate, {}, ["prisma", "modelNameLower"])
-);
+export function makeFindUnique({ modelName }: { modelName: string }) {
+    return makeQueryTemplate({
+        queryOperation: "findUnique",
+        modelName: modelName,
+        argsReturn: makeFindUniqueArgsTemplate({ modelName }),
+        type: "'#{modelName}'",
+        nullable: true,
+        resolve: makeFindUniqueResloverTemplate({
+            prismaName: "prisma",
+            modelName: modelName,
+        }),
+    });
+}
 
-export const queries = { findFirst, findMany, count, findUnique };
+export function makeCount({ modelName }: { modelName: string }) {
+    return makeQueryTemplate({
+        queryOperation: "count",
+        modelName: modelName,
+        argsReturn: makeQueryArgsTemplate({ modelName }),
+        type: "'Int'",
+        nullable: false,
+        resolve: makeQueryResloverTemplate({
+            prismaName: "prisma",
+            isPrisma: false,
+            modelName,
+            operation: "count",
+            hasDistinct: false,
+        }),
+    });
+}
+
+export const queries = {
+    makeFindFirst,
+    makeFindMany,
+    makeCount,
+    makeFindUnique,
+};
